e53de900d427a46bbc99ec25d616578465f23c56
Jeremey Schrack <jdschrack@gmail.com>
2023-05-07 08:56:25 -0400

    Setting up linux system

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "kanagawa-wave",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -33,6 +34,11 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
+          "cs",
+          "ts",
+          "tsx",
+          "js",
+          "jsx"
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
