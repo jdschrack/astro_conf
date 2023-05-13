fb1b8eda965a0a6e06fb33158b6e5578634e880f
Jeremey Schrack <jeremey.schrack@banno.com>
2023-05-13 10:29:39 -0400

    Fixing line numbering

--- options.lua
+++ options.lua
@@ -2,7 +2,7 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
@@ -18,12 +18,3 @@ return {
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
